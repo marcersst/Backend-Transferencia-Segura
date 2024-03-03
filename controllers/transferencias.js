f41e9e97a200eb0ec1d70a62c74e57d21f30c426
marcersst <104516883+marcersst@users.noreply.github.com>
2024-03-03 13:02:14 -0300

    Update transferencias.js

--- controllers/transferencias.js
+++ controllers/transferencias.js
@@ -44,7 +44,7 @@ const obtenerTransferencias = async (req, res = response) => {
         const filtro = {};
         if (id) filtro.id = id;
         if (creador) filtro.creador = creador;
-        if (destino) filtro.destino = destino;
+        if (destino) filtro.destino = destinatario;
        
        //arreglo en firmantes para unificar la busqueda en un solo criterio
         if (firmantes) 
